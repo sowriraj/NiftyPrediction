794b95b81a644713edaa227563b3876053a35791
sowriraj <msraj50@gmail.com>
2024-12-14 23:24:51 +0530

    Update Program.cs

--- NifTyPredictor/NifTyPredictor/Program.cs
+++ NifTyPredictor/NifTyPredictor/Program.cs
@@ -65,10 +65,10 @@ var app = builder.Build();
 //{
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-   app.UseHsts();
+ //  app.UseHsts();
 //}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
