fced9b8cfcadf335a445ea8d188198a515b120a7
sowriraj <msraj50@gmail.com>
2024-12-15 20:33:21 +0530

    Update Program.cs

--- NifTyPredictor/NifTyPredictor/Program.cs
+++ NifTyPredictor/NifTyPredictor/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddHttpClient("nseClient", client =>
 .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
 {
     CookieContainer = cookieContainer1,
-    UseProxy = true,
+    UseProxy = false,
     DefaultProxyCredentials = CredentialCache.DefaultCredentials
 });
 
@@ -52,7 +52,7 @@ builder.Services.AddHttpClient("nsebankClient", client =>
 .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
 {
     CookieContainer = cookieContainer2,
-    UseProxy = true,
+    UseProxy = false,
     DefaultProxyCredentials = CredentialCache.DefaultCredentials
 });
 
@@ -61,14 +61,14 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+//if (!app.Environment.IsDevelopment())
+//{
   app.UseExceptionHandler("/Error");
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-  app.UseHsts();
-}
+  //app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
